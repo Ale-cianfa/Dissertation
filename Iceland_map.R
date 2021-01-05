0428d8b7f17149d81439c3d33da5b708202a7b90
Ale-cianfa <s1751650@ed.ac.uk>
2021-01-05 16:25:48 +0100

    making map of iceland

--- Iceland_map.R
+++ Iceland_map.R
@@ -7,6 +7,7 @@ library(ggthemes) #for extra map themes
 library(viridis) #for the colors
 library(maps) #for the base map data
 library(hrbrthemes) #for the fonts in ggplot
+library(ggrepel)
 
 #things to include: general area, finnafjord
 
@@ -29,6 +30,35 @@ ice_map <- world %>%
     theme_minimal() + #choosing what type of background we want to display 
     ylim(62,68) +
     xlim(-27, -10) +
+    #ylim(65,68) + 
+    #xlim(-28, -9) + 
     coord_map())
 
-#trying to plot things on the map 
+#trying to plot things on the map 64.144555, -21.941021
+
+location <- c("Finnafjörður", "Reykjavík")
+lat <- c(66.116667, 64.144555)
+long <- c(-15.133333, -21.941021)
+group <- as.factor(c("a", "b"))
+
+imp_locations <- data.frame(location, lat, long, group)
+head(imp_locations)
+str(imp_locations)
+
+#Plotting Iceland
+(iceland <- ggplot() +
+    geom_polygon(data = ice_map, aes(x = long, y = lat, group = group) 
+                 , color = "black", fill = "#698B69", alpha = 0.6, size = 0.3) + #plot the data points on the map
+    geom_point(data = imp_locations, aes(x = long, y = lat, colour = group), 
+               colour = c("#0E4749", "#CC978E"), size = 3) +
+    theme_minimal() + 
+    geom_label_repel(data = imp_locations, aes(x = long, y = lat,
+                                                     label = location),
+                     box.padding = 5, size = 5, alpha = 0.9, nudge_x = 1,
+                     min.segment.length = 0, inherit.aes = FALSE) + 
+    ylim(62,68) +
+    xlim(-27, -10) +
+    coord_map())
+
+
+

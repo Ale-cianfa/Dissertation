ca8df3940d3d7c11c1000577493d27ad33c4929a
Ale-cianfa <s1751650@ed.ac.uk>
2021-04-08 23:23:36 +0100

    ale joined the datasets

--- general_script.R
+++ general_script.R
@@ -302,14 +302,55 @@ survey_15 <- survey %>%
 write_csv(survey_15, file = "NASS/intermediate surveys/by_year/survey_15.csv")
 
 ## Putting together csvs from the different years----
+
 complete_01 <- read_csv("NASS/qgis_survey/2001_complete.csv")
 complete_07 <- read_csv("NASS/qgis_survey/2007_complete.csv")
 complete_15 <- read_csv("NASS/qgis_survey/2015_complete.csv")
 
-## Working on dataset 2001
+str(complete_01)
 
+## Working on dataset 2001 (renaming the columns)
 
-#prova_final <- rbind(complete_01, complete_07, complete_15) #you have to make sure to save them in the right order otherwise the name do not match
+complete_01 <- complete_01 %>% 
+   rename(chlorMarch = chlor0103, chlorApril = chlor0104, 
+          chlorMay = chlor0105, chlorJune = chlor0106, 
+          chlorJuly = chlor0107, chlorAug = chlor0108,
+          mldMarch = mlotst0103, mldApril = mlotst0104,
+          mldMay = mlotst0105, mldJune = mlotst0106, 
+          mldJuly = mlotst0107, mldAug = mlotst0108, 
+          sstMarch = sst0103, sstApril = sst0104,
+          sstMay = sst0105, sstJune = sst0106, 
+          sstJuly = sst0107, sstAug = sst0108)
+         
+## Working on dataset 2007 (renaming the columns)
+
+complete_07 <- complete_07 %>% 
+   rename(chlorMarch = chlor0703, chlorApril = chlor0704, 
+          chlorMay = chlor0705, chlorJune = chlor0706, 
+          chlorJuly = chlor0707, chlorAug = chlor0708,
+          mldMarch = mlotst0703, mldApril = mlotst0704,
+          mldMay = mlotst0705, mldJune = mlotst0706, 
+          mldJuly = mlotst0707, mldAug = mlotst0708, 
+          sstMarch = sst0703, sstApril = sst0704,
+          sstMay = sst0705, sstJune = sst0706, 
+          sstJuly = sst0707, sstAug = sst0708)
+
+## Working on dataset 2015 (renaming the columns)
+
+complete_15 <- complete_15 %>% 
+   rename(chlorMarch = chlor1503, chlorApril = chlor1504, 
+          chlorMay = chlor1505, chlorJune = chlor1506, 
+          chlorJuly = chlor1507, chlorAug = chlor1508,
+          mldMarch = mlotst1503, mldApril = mlotst1504,
+          mldMay = mlotst1505, mldJune = mlotst1506, 
+          mldJuly = mlotst1507, mldAug = mlotst1508, 
+          sstMarch = sst1503, sstApril = sst1504,
+          sstMay = sst1505, sstJune = sst1506, 
+          sstJuly = sst1507, sstAug = sst1508)
+
+## Binding the data sets per year----
+
+prova_final <- rbind(complete_01, complete_07, complete_15) #you have to make sure to save them in the right order otherwise the name do not match
 
 
 

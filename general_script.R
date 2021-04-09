c1587e911eff3bcc8df4b9e91c77f9de606016a3
Ale-cianfa <s1751650@ed.ac.uk>
2021-04-09 09:55:18 +0100

    ale is committing new script and changes to general one

--- general_script.R
+++ general_script.R
@@ -6,6 +6,7 @@ library(tidyverse)
 library(ggplot2)
 library(viridis)
 library(sf)
+library(lme4) #for mixed models
 library(maps) #for the base map data
 library(hrbrthemes) #for the fonts in ggplot
 library(ggrepel)
@@ -14,10 +15,11 @@ library(raster)
 library(rgdal)
 library(rasterVis)
 library(sp)
+library(mgcv)
 
 getwd()
 
-## NASS survey----
+## NASS SURVEY----
 survey <- read.csv("NASS/original/orignal_ale_nass-sightings_tot.csv")
 head(survey)
 str(survey)
@@ -28,7 +30,7 @@ vessels <- read.csv("NASS/original/nass_vessels.csv")
 
 unique(survey$vID) #TOTAL NUMBER OF VESSELS
 
-## Iceland map----
+## ICELAND MAP----
 
 ### Importing world map and checking everything is alright 
 world <- map_data("world")
@@ -352,11 +354,38 @@ complete_15 <- complete_15 %>%
 
 prova_final <- rbind(complete_01, complete_07, complete_15) #you have to make sure to save them in the right order otherwise the name do not match
 
+## FAKE GAM----
 
+# fake data
+n <- 50
+sig <- 2
+dat <- gamSim(1,n=n,scale=sig)
 
+# P-spline smoothers (with lambda=0.6) used for x1 and x2; x3 is parametric.
+b1 <- mgcv::gam(y ~ s(x1, bs='ps', sp=0.6) + s(x2, bs='ps', sp=0.6) + x3, data = dat)
+summary(b1)
+plot(b1)
 
+# plot the smooth predictor function for x1 with ggplot to get a nicer looking graph
+p <- predict(b1, type="lpmatrix")
+beta <- coef(b1)[grepl("x1", names(coef(b1)))]
+s <- p[,grepl("x1", colnames(p))] %*% beta
+ggplot(data=cbind.data.frame(s, dat$x1), aes(x=dat$x1, y=s)) + geom_line()
 
+# predict
+newdf <- gamSim(1,n=n,scale=sig)
+f <- predict(b1, newdata=newdf)
 
+# select smoothing parameters with REML, using P-splines
+b2 <- mgcv::gam(y ~ s(x1, bs='ps') + s(x2, bs='ps') + x3, data = dat, method="REML")
+
+# select variables and smoothing parameters
+b3 <- mgcv::gam(y ~ s(x0) + s(x1) + s(x2) + s(x3) , data = dat, method="REML", select=TRUE)
+
+# loess smoothers with the gam package (restart R before loading gam)
+library(gam)
+b4 <- gam::gam(y ~ lo(x1, span=0.6) + lo(x2, span=0.6) + x3, data = dat)
+summary(b4)
 
 
 

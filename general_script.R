1fcdb6f0e3d101a7a8c272e8b0b7897f0394073a
Ale-cianfa <s1751650@ed.ac.uk>
2021-04-11 11:58:52 +0100

    ale committing stuff

--- general_script.R
+++ general_script.R
@@ -139,137 +139,81 @@ plot(bathymetry)
 
 display.brewer.all()
 
-bat_df <- as.data.frame(bathymetry, xy = TRUE, na.rm = TRUE)
+#bat_df <- as.data.frame(bathymetry, xy = TRUE, na.rm = TRUE)
 
-bat_df <- bat_df %>% filter(bat_1 < 0)
+#bat_df <- bat_df %>% filter(bat_1 < 0)
 
-(bat_plot <- ggplot() +
-      geom_raster(data = bat_df, aes(x = x, y = y, fill = bat_1)) +
-      #scale_fill_viridis_c() +
-      scale_color_brewer(palette = "BuPu") +
-      coord_quickmap() +
-      ggtitle("North of Iceland Bathymetric profile") +
-      ylim(64.5,68) +
-      xlim(-27, -10) +
-      theme_classic() +
-      theme(legend.position = "right",
-            legend.title = element_text(size = 13, face ="bold"),
-            legend.text = element_text(size = 12)) + # removes defalut grey background
-      theme(plot.title = element_text(size = 15, face ="bold", hjust = 0.5),             # centres plot title
-            text = element_text(size=20),		       	    # font size
-            axis.text.x = element_text(angle = 90, hjust = 1)) +
-      labs(fill = "Depth (m)", 
-           x = "longitude", 
-           y ="latitude")) # rotates x axis text
+#(bat_plot <- ggplot() +
+      #geom_raster(data = bat_df, aes(x = x, y = y, fill = bat_1)) +
+#      scale_fill_viridis_c() +
+#     coord_quickmap() +
+#     ggtitle("Bathymetric profile of the North of Iceland") +
+#      ylim(64.5,68) +
+#      xlim(-27, -10) +
+#      theme_classic() +
+#      theme(legend.position = "right",
+#            legend.title = element_text(size = 13, face ="bold"),
+#             legend.text = element_text(size = 12)) + # removes defalut grey background
+#      theme(plot.title = element_text(size = 15, face ="bold", hjust = 0.5),             # centres plot title
+#            text = element_text(size=20),		       	    # font size
+#            axis.text.x = element_text(angle = 90, hjust = 1)) +
+#      labs(fill = "Depth (m)", 
+#           x = "Longitude", 
+#           y ="Latitude")) # rotates x axis text
 
-ggsave(bat_plot, file = "img/bat_plot3.png", height = 5, width = 9)
+#ggsave(bat_plot, file = "img/bat_plot4.png", height = 5, width = 9)
 
 ### Chlorophyll:
 
-chlor_0103 <- raster("Parameters/chlor_a/chlor_0103.tif")
-chlor_0104 <- raster("Parameters/chlor_a/chlor_0104.tif")
-chlor_0105 <- raster("Parameters/chlor_a/chlor_0105.tif")
-chlor_0106 <- raster("Parameters/chlor_a/chlor_0106.tif")
 chlor_0107 <- raster("Parameters/chlor_a/chlor_0107.tif")
-chlor_0108 <- raster("Parameters/chlor_a/chlor_0108.tif")
 
 ## Creating a data frame for each year to show the progress in a facet plot
 
-chlor_0103_df <- as.data.frame(chlor_0103, xy = TRUE, na.rm = TRUE)
-chlor_0104_df <- as.data.frame(chlor_0104, xy = TRUE, na.rm = TRUE)
-chlor_0105_df <- as.data.frame(chlor_0105, xy = TRUE, na.rm = TRUE)
-chlor_0106_df <- as.data.frame(chlor_0106, xy = TRUE, na.rm = TRUE)
 chlor_0107_df <- as.data.frame(chlor_0107, xy = TRUE, na.rm = TRUE)
-chlor_0108_df <- as.data.frame(chlor_0108, xy = TRUE, na.rm = TRUE)
 
+## Making the plot 
 
-## Making the facet plot 
-
-(chlor_facet_01_03 <- ggplot() +
-   geom_raster(data = chlor_0103_df, aes(x = x, y = y, fill = chlor_0103)) +
-   scale_fill_viridis_c() +
-   coord_quickmap() +
-   ggtitle("Chlorophyll") +
-   
-   xlab("Longitude") +
-   ylab("Latitude") +
-   ylim(62,68) +
-   xlim(-27, -10) +
-   theme_classic() +   					    # removes defalut grey background
-   theme(plot.title = element_text(hjust = 0.5),             # centres plot title
-         text = element_text(size=20),		       	    # font size
-         axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
-
-(chlor_facet_01_04 <- ggplot() +
-      geom_raster(data = chlor_0104_df, aes(x = x, y = y, fill = chlor_0104)) +
-      scale_fill_viridis_c() +
-      coord_quickmap() +
-      ggtitle("Chlorophyll") +
-      xlab("Longitude") +
-      ylab("Latitude") +
-      ylim(62,68) +
-      xlim(-27, -10) +
-      theme_classic() +   					    # removes defalut grey background
-      theme(plot.title = element_text(hjust = 0.5),             # centres plot title
-            text = element_text(size=20),		       	    # font size
-            axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
-
-(chlor_facet_01_05 <- ggplot() +
-      geom_raster(data = chlor_0105_df, aes(x = x, y = y, fill = chlor_0105)) +
-      scale_fill_viridis_c() +
-      coord_quickmap() +
-      ggtitle("Chlorophyll") +
-      xlab("Longitude") +
-      ylab("Latitude") +
-      ylim(62,68) +
-      xlim(-27, -10) +
-      theme_classic() +   					    # removes defalut grey background
-      theme(plot.title = element_text(hjust = 0.5),             # centres plot title
-            text = element_text(size=20),		       	    # font size
-            axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
-
-(chlor_facet_01_06 <- ggplot() +
-      geom_raster(data = chlor_0106_df, aes(x = x, y = y, fill = chlor_0106)) +
-      scale_fill_viridis_c() +
-      coord_quickmap() +
-      ggtitle("Chlorophyll") +
-      xlab("Longitude") +
-      ylab("Latitude") +
-      ylim(62,68) +
-      xlim(-27, -10) +
-      theme_classic() +   					    # removes defalut grey background
-      theme(plot.title = element_text(hjust = 0.5),             # centres plot title
-            text = element_text(size=20),		       	    # font size
-            axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
-
-
-(chlor_facet_01_07 <- ggplot() +
+(chlor_01_07 <- ggplot() +
       geom_raster(data = chlor_0107_df, aes(x = x, y = y, fill = chlor_0107)) +
-      scale_fill_viridis_c() +
+      scale_fill_viridis_c()+
       coord_quickmap() +
-      ggtitle("Chlorophyll") +
+      ggtitle("Chlorophyll map of Northern Iceland, July 2001") +
+      ylim(64.5,68) +
+      xlim(-27, -10) +
+      theme_classic() + # removes defalut grey background
+   theme(legend.position = "right",
+         legend.title = element_text(size = 13, face ="bold"),
+         legend.text = element_text(size = 12)) + # removes defalut grey background
+         theme(plot.title = element_text(size = 15, face ="bold", hjust = 0.5),             # centres plot title
+         text = element_text(size=12),		# font size
+         axis.text.x = element_text(angle = 90, hjust = 1)) +      
+      labs(fill = "Chlorophyll-a \n(mg m-3)", 
+           x = "Longitude", 
+           y = "Latitude"))
+
+      #set this as theme!! 
+
+#ggsave(chlor_01_07, file = "img/chlor_ex.png", height = 5, width = 9)
+
+chlor_1507 <- raster("Parameters/chlor_a/chlor_1507.tif")
+
+chlor_1507_df <- as.data.frame(chlor_1507, xy = TRUE, na.rm = TRUE)
+
+(chlor_15_07 <- ggplot() +
+      geom_raster(data = chlor_1507_df, aes(x = x, y = y, fill = chlor_1507)) +
+      scale_fill_viridis_c()+
+      coord_quickmap() +
+      ggtitle("Chlorophyll map of Northern Iceland, July 2015") +
       xlab("Longitude") +
       ylab("Latitude") +
-      ylim(62,68) +
+      ylim(64.5,68) +
       xlim(-27, -10) +
       theme_classic() +   					    # removes defalut grey background
       theme(plot.title = element_text(hjust = 0.5),             # centres plot title
             text = element_text(size=20),		       	    # font size
             axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
 
-(chlor_facet_01_08 <- ggplot() +
-      geom_raster(data = chlor_0108_df, aes(x = x, y = y, fill = chlor_0108)) +
-      scale_fill_viridis_c(direction = 1) +
-      coord_quickmap() +
-      ggtitle("Chlorophyll") +
-      xlab("Longitude") +
-      ylab("Latitude") +
-      ylim(62,68) +
-      xlim(-27, -10) +
-      theme_classic() +   					    # removes defalut grey background
-      theme(plot.title = element_text(hjust = 0.5),             # centres plot title
-            text = element_text(size=20),		       	    # font size
-            axis.text.x = element_text(angle = 90, hjust = 1)))  # rotates x axis text
+
 
 ## Trying to make a facet plot 
 #attach(mtcars)
